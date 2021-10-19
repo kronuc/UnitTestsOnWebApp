871f68efa23aea6d24fae940e9191f41c7de1139
kronuc <denerator2@gmail.com>
2021-10-19 12:07:16 +0300

    add arrange act assert comments

--- UnitTestsOnWebApp.Tests/HomeControllerTest.cs
+++ UnitTestsOnWebApp.Tests/HomeControllerTest.cs
@@ -16,49 +16,61 @@ namespace UnitTestsOnWebApp.Tests
         [Fact]
         public void Index_ExecuteIndexWithNoParametr_ResultTypeIsViewResult()
         {
+            // arrange
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetAll().Returns(new List<User>());
             HomeController homeController = new HomeController(userRepository);
 
+            //act
             var actual = homeController.Index();
 
+            // assert
             actual.Should().BeOfType<ViewResult>();
         }
 
         [Fact]
         public void Index_ExecuteIndexWithNoParametr_ModelTypeIsList()
         {
+            // arrange
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetAll().Returns(new List<User>());
             HomeController homeController = new HomeController(userRepository);
 
+            // act
             var actual = homeController.Index();
 
+            // assert
             actual.As<ViewResult>().Model.Should().BeOfType<List<User>>();
         }
 
         [Fact]
         public void Index_ExecuteIndexWithRandomParametr_ResultTypeIsViewResult()
         {
+            // arrange
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetUserRange(Arg.Any<int>(), Arg.Any<int>()).Returns(new List<User>());
             HomeController homeController = new HomeController(userRepository);
 
+            // act
             var actual = homeController.Index(AutoFaker.Generate<int>(), AutoFaker.Generate<int>());
 
+            // assert
             actual.Should().BeOfType<ViewResult>();
         }
 
         [Fact]
         public void Index_ExecuteIndexWithRandomParametr_ModelTypeIsList()
         {
+            // arrange
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetUserRange(Arg.Any<int>(), Arg.Any<int>()).Returns(new List<User>());
             HomeController homeController = new HomeController(userRepository);
 
 
+            // act
             var actual = homeController.Index(AutoFaker.Generate<int>(), AutoFaker.Generate<int>());
 
+            // assert
             actual.As<ViewResult>().Model.Should().BeOfType<List<User>>();
         }
 
@@ -67,14 +79,17 @@ namespace UnitTestsOnWebApp.Tests
         [InlineData(3)]
         public void Index_ExecuteIndexWithNoParametr_ResultLenghtSameAsRepositoryResponce(int inputDatataLenght)
         {
+            // arrange
             List<User> dataSource = DataGeneration(inputDatataLenght);
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetAll().Returns(dataSource);
             HomeController homeController = new HomeController(userRepository);
             int expected = inputDatataLenght;
-            
+             
+            // act
             var actual = homeController.Index();
 
+            // assert
             actual.As<ViewResult>().Model.As<List<User>>().Count.Should().Be(expected);
         }
 
@@ -83,14 +98,17 @@ namespace UnitTestsOnWebApp.Tests
         [InlineData(3)]
         public void Index_ExecuteIndexWithNoParametr_ResultListSameAsRepositoryResponce(int inputDatataLenght)
         {
+            // arrange
             List<User> dataSource = DataGeneration(inputDatataLenght);
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetAll().Returns(dataSource);
             HomeController homeController = new HomeController(userRepository);
             var expected = dataSource;
 
+            // act
             var actual = homeController.Index();
 
+            // assert
             actual.As<ViewResult>().Model.As<List<User>>().Should().BeEquivalentTo(expected);
         }
 
@@ -99,14 +117,17 @@ namespace UnitTestsOnWebApp.Tests
         [InlineData(0, -1, 2)]
         public void Index_ExecuteIndexWithRandomParametr_ResultListSameAsRepositoryResponce(int inputDatataLenght, int start, int end)
         {
+            // arrange
             List<User> dataSource = DataGeneration(inputDatataLenght);
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetUserRange(Arg.Any<int>(), Arg.Any<int>()).Returns(dataSource);
             HomeController homeController = new HomeController(userRepository);
             var expected = dataSource;
 
+            // act
             var actual = homeController.Index(start, end);
 
+            // assert
             actual.As<ViewResult>().Model.As<List<User>>().Should().BeEquivalentTo(expected);
         }
         [Theory]
@@ -114,15 +135,17 @@ namespace UnitTestsOnWebApp.Tests
         [InlineData(0, -1, 2)]
         public void Index_ExecuteIndexWithNoParametr_ResultListAsContainsApropriateItemsFromDataSource(int inputDatataLenght, int start, int end)
         {
+            // arrange
             List<User> dataSource = DataGeneration(inputDatataLenght);
             IUserRepository userRepository = Substitute.For<IUserRepository>();
             userRepository.GetUserRange(Arg.Any<int>(), Arg.Any<int>()).Returns(dataSource);
             HomeController homeController = new HomeController(userRepository);
             var expected = dataSource;
 
-
+            // act
             var actual = homeController.Index(start, end);
 
+            // assert
             actual.As<ViewResult>().Model.As<List<User>>().Should().BeEquivalentTo(expected);
         }
 
